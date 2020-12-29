1b843dc7c1f121198041cc45cb28c14d4206aa49
Matt Gerry <codingwiththeforce@gmail.com>
2020-12-29 02:12:20 -0600

    Updated the test shell script

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -11,4 +11,4 @@ echo "Creating the Scratch Org..."
 sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
 
 sfdx force:source:push -u ${CIRCLE_BRANCH}
-sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername ${CIRCLE_BRANCH} 
+sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername ${CIRCLE_BRANCH}

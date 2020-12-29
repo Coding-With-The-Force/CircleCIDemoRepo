e7e733196651532b0955b72b957e19ad37ae0b2f
Matt Gerry <codingwiththeforce@gmail.com>
2020-12-29 01:30:34 -0600

    Updated the test shell script

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -1,2 +1,5 @@
-sfdx force:source:push -u DevHub
-sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername DevHub
+echo "Coverting source to metadata format"
+sfdx force:source:convert -d test_code -r force-app
+
+echo "Deploying code to org"
+sfdx force:mdapi:deploy --checkonly -u DevHub -d test_code/ -w -1 -l RunLocalTests

bab2300e62aeaad5fd2495d94fbe00d8cff17a81
Matt Gerry <codingwiththeforce@gmail.com>
2020-12-29 10:41:08 -0600

    Updated the test shell script

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -1,14 +1,16 @@
-echo "Logging into Salesforce Org"
-mkdir keys
-echo $CERT_KEY | base64 -di > keys/server.key
-
 # Authenticate to salesforce
-echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
+#echo "Authenticating..."
+#sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
 
 #Create a scratch org
-echo "Creating the Scratch Org..."
-sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
+#echo "Creating the Scratch Org..."
+#sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
 
-sfdx force:source:push -u ${CIRCLE_BRANCH}
-sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername ${CIRCLE_BRANCH}
+#sfdx force:source:push -u ${CIRCLE_BRANCH}
+sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername DevHub
+
+#echo "Coverting source to metadata format"
+#sfdx force:source:convert -d test_code -r force-app
+
+#echo "Deploying code to org"
+#sfdx force:mdapi:deploy --checkonly -u DevHub -d test_code/ -w -1 -l RunLocalTests

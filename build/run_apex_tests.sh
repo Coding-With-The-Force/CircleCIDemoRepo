7787e413ec1a1f9ff33d570f429ba7757142661f
Matt Gerry <codingwiththeforce@gmail.com>
2020-12-29 01:53:58 -0600

    Updated the test shell script

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -1,3 +1,7 @@
+echo "Logging into Salesforce Org"
+mkdir keys
+echo $CERT_KEY | base64 -di > keys/server.key
+
 # Authenticate to salesforce
 echo "Authenticating..."
 sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub

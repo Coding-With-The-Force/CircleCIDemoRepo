ac126586708d8c28dd59f4acb541780639717c41
Matt Gerry <codingwiththeforce@gmail.com>
2020-12-29 19:59:22 -0600

    Commented code for demo

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -7,11 +7,15 @@
 #sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
 
 #sfdx force:source:push -u ${CIRCLE_BRANCH}
-sfdx force:source:deploy --sourcepath force-app --targetusername DevHub
-sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername DevHub
 
 #echo "Coverting source to metadata format"
 #sfdx force:source:convert -d test_code -r force-app
 
 #echo "Deploying code to org"
 #sfdx force:mdapi:deploy --checkonly -u DevHub -d test_code/ -w -1 -l RunLocalTests
+
+echo "Deploying source to org"
+sfdx force:source:deploy --sourcepath force-app --targetusername DevHub
+
+echo "Testing code in org"
+sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername DevHub
